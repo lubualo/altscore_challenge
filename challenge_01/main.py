aea3d84f378efae2b2c6ffd8a365b17ee0aef376
lubualo@gmail.com <lubualo@gmail.com>
2025-05-17 16:58:25 -0300

    Challenge 01 - La sonda silenciosa -  solved

---
+++ challenge_01/main.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+import os
+from common.ApiClient import ApiClient
+import re
+
+def main():
+    load_dotenv()
+    api_key = os.getenv("API_KEY")
+    base_url = os.getenv("BASE_URL")
+    api_client = ApiClient()
+    get_url = base_url + "/v1/s1/e1/resources/measurement"
+    post_url = base_url + "/v1/s1/e1/solution"
+    response = api_client.get(get_url)
+    while (response["distance"] == 'failed to measure, try again' and response["time"] == 'failed to measure, try again'):
+        response = api_client.get(get_url)
+
+    print(response)
+    # Possible improvement: use a dataclass instead of a dictionary for the api response
+    distance = convertStringMeasuermentToFloat(response["distance"])
+    time = convertStringMeasuermentToFloat(response["time"])
+    speed = distance // time
+    data = {"speed": str(speed)}
+    print(data)
+    response = api_client.post(post_url, data)
+    print(response)
+
+def convertStringMeasuermentToFloat(timeString):
+    match = re.search(r"([\d.]+)", timeString)
+    return float(match.group(1))
+
+
+if __name__ == "__main__":
+    main()
---
+++ common/ApiClient.py
@@ -0,0 +1,37 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class ApiClient:
+    def __init__(self):
+        self.api_key = os.getenv("API_KEY")
+
+    def _get_headers(self, extra_headers=None):
+        headers = {
+            "API-KEY": self.api_key  # Custom header expected by the API
+        }
+        if extra_headers:
+            headers.update(extra_headers)
+        return headers
+
+    def get(self, url, headers=None):
+        try:
+            response = requests.get(url, headers=self._get_headers(headers))
+            if response.status_code == 200:
+                return response.json()
+            else:
+                raise Exception(f"GET request failed: {response.status_code}, {response.text}")
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"An error occurred while making the GET call: {e}")
+
+    def post(self, url, data=None, headers=None):
+        try:
+            response = requests.post(url, json=data, headers=self._get_headers(headers))
+            if response.status_code in [200, 201]:
+                return response.json()
+            else:
+                raise Exception(f"POST request failed: {response.status_code}, {response.text}")
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"An error occurred while making the POST call: {e}")
