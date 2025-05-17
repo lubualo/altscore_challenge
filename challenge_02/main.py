c08b9c0c8a17df084d87978a639be2fa98cc7421
lubualo@gmail.com <lubualo@gmail.com>
2025-05-17 17:38:06 -0300

    Solved challenge 2

--- challenge_02/main.py
+++ challenge_02/main.py
@@ -9,7 +9,7 @@ def main():
     get_url = base_url + "/v1/s1/e2/resources/stars"
     solution_post_url = base_url + "/v1/s1/e2/solution"
 
-    # The resonance of a star is fixed from the previous one
+    # The resonance of a star is fixed from the previous one. As they are evenly spaced the midpoint is the average
     params = {
         "page": 1,
         "sort-by": "resonance",
@@ -17,10 +17,7 @@ def main():
     }
     response = api_client.get(get_url, params)
     min_resonance = response[0]["resonance"];
-    next_resonance = response[1]["resonance"];
-    step = next_resonance - min_resonance
     print("Min resonance: " + str(min_resonance))
-    print("Step: " + str(step))
     
     params = {
         "page": 1,
@@ -31,13 +28,8 @@ def main():
     max_resonance = response[0]["resonance"];
     print("Max resonance: " + str(max_resonance))
 
-    sum = 0
-    n = 0
-    for i in range(min_resonance, max_resonance + 1, step):
-        sum += i
-        n += 1
+    average_resonance = round((min_resonance + max_resonance) / 2)
 
-    average_resonance = sum // n
     print("Average resonance: " + str(average_resonance))
 
     response = api_client.post(solution_post_url, {"average_resonance": average_resonance})
